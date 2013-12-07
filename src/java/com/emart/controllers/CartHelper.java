e7a0903ad073086a3cbe105e88035b1b1446ac1a
Maciej <mwarch1@uic.edu>
2013-12-07 14:46:01 -0600

    Fixed subtotal problem

--- src/java/com/emart/controllers/CartHelper.java
+++ src/java/com/emart/controllers/CartHelper.java
@@ -3,46 +3,48 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package com.emart.controllers;
 
 import com.pojos.Product;
 import com.pojos.Productprices;
+import java.math.BigDecimal;
 import java.util.Iterator;
 import java.util.Map;
-
 
 /**
  *
  * @author punker
  */
-
 public class CartHelper {
-    
+
     public CartHelper() {
-        
+
     }
-    
+
     public float getProductPrice(Product p) {
-       System.out.println(p);
-       float price = ((Productprices)p.getProductpriceses().iterator().next()).getProducPrice();
-       
-       return price;
+        System.out.println(p);
+        float price = ((Productprices) p.getProductpriceses().iterator().next()).getProducPrice();
+
+        return price;
     }
-    
+
     public float getSubtotal(Map<Integer, Product> items_map, Map<Integer, Integer> qty_map) {
-        float subtotal = 0;
-        
+        BigDecimal subtotal = new BigDecimal(0);
+        subtotal.setScale(1, BigDecimal.ROUND_HALF_UP);
+
         Iterator it = items_map.entrySet().iterator();
-        while(it.hasNext()) {
-            Map.Entry pair = (Map.Entry)it.next();
-            int prod_id = (Integer)pair.getKey();
-            Product p = (Product)pair.getValue();
+        while (it.hasNext()) {
+            Map.Entry pair = (Map.Entry) it.next();
+            int prod_id = (Integer) pair.getKey();
+            Product p = (Product) pair.getValue();
             int qty = qty_map.get(prod_id);
-            subtotal = subtotal + (getProductPrice(p) * qty);
-            
+            float price = getProductPrice(p);
+            BigDecimal temp = new BigDecimal(price * qty);
+            subtotal = subtotal.add(temp);
+
         }
-        
-        return subtotal;
+        subtotal = subtotal.setScale(2, BigDecimal.ROUND_HALF_UP);
+        System.out.println(subtotal.toString());
+        return subtotal.floatValue();
     }
 }
