4f946f8f876e29985f1b957097194a3a4db3d594
gvarun22 <gvarun22@gmail.com>
2013-12-07 12:35:31 -0600

    Added Junit tests for My code

---
+++ test/HomeActionTest.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import com.emart.HomeAction;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import junit.framework.Assert;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Varun
+ */
+public class HomeActionTest {
+
+    public HomeActionTest() {
+    }
+
+    @Test
+    public void Success_Recommendation_not_empty_HomeActionTest() {
+        try {
+            HomeAction ha = new HomeAction();
+            String result = ha.execute();
+            Assert.assertEquals("success", result);
+            Assert.assertNotNull(ha.getPopularItems());
+            Assert.assertNotSame(0, ha.getPopularItems().size());
+        } catch (Exception ex) {
+            Logger.getLogger(HomeActionTest.class.getName()).log(Level.SEVERE, null, ex);
+            fail("Recommendation failed");
+        }
+
+    }
+}
+
---
+++ test/SearchItemActionTest.java
@@ -0,0 +1,82 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import com.emart.SearchItemAction;
+import com.pojos.Product;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.junit.Assert;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Varun
+ */
+public class SearchItemActionTest {
+
+    public SearchItemActionTest() {
+
+    }
+
+    @Test
+    public void Success_Category_SearchItemActionTest() {
+        try {
+            SearchItemAction sia = new SearchItemAction();
+            sia.setName("HP");
+            sia.setProdCategory("Electronics");
+            String result = sia.execute();
+            Product p = (Product) sia.getNewList().get(0);
+            Assert.assertEquals("success", result);
+            Assert.assertEquals("HP Pavilion Laptop", p.getProductName());
+        } catch (Exception ex) {
+            Logger.getLogger(SearchItemActionTest.class.getName()).log(Level.SEVERE, null, ex);
+            fail("Search did not succeed");
+        }
+    }
+
+    @Test
+    public void Success_No_Category_SearchItemActionTest() {
+        try {
+            SearchItemAction sia = new SearchItemAction();
+            sia.setName("HP");
+            String result = sia.execute();
+            Product p = (Product) sia.getNewList().get(0);
+            Assert.assertEquals("success", result);
+            Assert.assertEquals("HP Pavilion Laptop", p.getProductName());
+        } catch (Exception ex) {
+            Logger.getLogger(SearchItemActionTest.class.getName()).log(Level.SEVERE, null, ex);
+            fail("Search did not succeed");
+        }
+    }
+
+    @Test
+    public void Failure_No_Category_SearchItemActionTest() {
+        try {
+            SearchItemAction sia = new SearchItemAction();
+            sia.setName("HIP");
+            String result = sia.execute();
+            Assert.assertEquals("error", result);
+        } catch (Exception ex) {
+            Logger.getLogger(SearchItemActionTest.class.getName()).log(Level.SEVERE, null, ex);
+            fail("Search did not succeed");
+        }
+    }
+
+    @Test
+    public void Failure_Category_SearchItemActionTest() {
+        try {
+            SearchItemAction sia = new SearchItemAction();
+            sia.setName("HIP");
+            sia.setProdCategory("Ji Goe");
+            String result = sia.execute();
+            Assert.assertEquals("error", result);
+        } catch (Exception ex) {
+            Logger.getLogger(SearchItemActionTest.class.getName()).log(Level.SEVERE, null, ex);
+            fail("Search did not succeed");
+        }
+    }
+}
